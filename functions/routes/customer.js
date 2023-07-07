c63073472518e23d0684662278e8a3637b4db0f8
chaitanya <Chaitanya.Katepalli@vitalitygroup.com>
2023-07-07 12:11:39 +0530

    basic Api's

--- functions/routes/customer.js
+++ functions/routes/customer.js
@@ -1,1 +1,8 @@
 //customer related api's
+const router = require('express').Router();
+
+router.get('/', function(req, res) {
+    res.send('Customers list')
+})
+
+module.exports = router
--- functions/routes/user.js
+++ functions/routes/user.js
@@ -1,1 +1,8 @@
 //user related api's
+const router = require('express').Router();
+
+router.get('/', function(req, res) {
+    res.send('users list')
+})
+
+module.exports = router
